8b498cc1c001986ba8fca986109627e81e454ad7
scottharris86 <scottharrisdev@gmail.com>
2020-02-11 12:59:29 -0500

    basic table view set up and ell working

--- FindACrew/View Controllers/PersonSearchTableViewController.swift
+++ FindACrew/View Controllers/PersonSearchTableViewController.swift
@@ -9,5 +9,22 @@
 import UIKit
 
 class PersonSearchTableViewController: UITableViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath) as? PersonTableViewCell else { return UITableViewCell() }
+        
+        cell.genderLabel.text = "Male"
+        cell.nameLabel.text = "Scott"
+        cell.birthYearLabel.text = "1986"
+        
+        
+        return cell
+    }
     
 }
---
+++ FindACrew/Views/Cells/PersonTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonTableViewCell.swift
+//  FindACrew
+//
+//  Created by scott harris on 2/11/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var birthYearLabel: UILabel!
+    
+}
