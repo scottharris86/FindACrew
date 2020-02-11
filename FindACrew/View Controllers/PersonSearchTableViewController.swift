37dc668a3b012f9bc324cdd279552ddd243700cf
scottharris86 <scottharrisdev@gmail.com>
2020-02-11 12:14:30 -0500

    initial project setup

---
+++ FindACrew/View Controllers/PersonSearchTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PersonSearchTableViewController.swift
+//  FindACrew
+//
+//  Created by scott harris on 2/11/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class PersonSearchTableViewController: UITableViewController {
+    
+}
